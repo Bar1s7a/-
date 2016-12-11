60e81d2bea61573f756fc285231fcbb825132b5a
Bar1s7a <kasimov9898@mail.ru>
2016-12-11 17:58:12 +0400

    Add files via upload

---
+++ Дз1.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+int a, b =0, c = 1;
+scanf("%d", &a);
+    while ( a > 0 ){
+		b = b + a%10*c;
+		c *=2;
+		a = a / 10;
+}
+printf("%d", b);
+system ("pause");
+return 0;
+}
