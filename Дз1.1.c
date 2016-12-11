46b43ec1802d18fc85ff57857db0e3005716e8dd
Bar1s7a <kasimov9898@mail.ru>
2016-12-11 18:15:29 +0400

    Update Дз1.1.c

--- Дз1.1.c
+++ Дз1.1.c
@@ -2,12 +2,12 @@
 
 int main() {
 int a;
-scanf("%d",&a);
-if (a>0){
-    printf("%d",2*a);
+scanf("%d", &a);
+if (a > 0) {
+    printf("%d", 2*a);
 }
-else if (a<0) {
-    printf("%d",a*3);
+else if (a < 0) {
+    printf("%d", a*3);
 }   else printf("10");
 return 0;
 }
